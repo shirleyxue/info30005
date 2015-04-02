53798aa4d585b1fc144f56853b64e2078e0f3f6d
David Olsen <dave@ua-unistudent-ten-9-194-63.uniaccess.unimelb.edu.au>
2015-04-02 14:55:27 +1100

    salty

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class User(db.Model):
     last_name = db.Column(db.String(64))
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(120))
+    salt = db.Column(db.String(120))
 
 
     # create the user 
